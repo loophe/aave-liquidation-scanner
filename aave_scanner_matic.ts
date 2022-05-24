703a14adc040098aca904bb6f6a3ba4bcee4f95b
loophe <loophe@163.com>
2022-05-25 01:30:23 +0800

    poly scanner added

--- aave_scanner_matic.ts
+++ aave_scanner_matic.ts
@@ -1,164 +1,167 @@
 import { request, gql } from "graphql-request";
 
-let lastTimeStamp = ~~(new Date().getTime() / 1000);
-// let user_id = "0xf37680f16b92747ee8537a7e2ccb0e51a7c52a64"
-let user_id = ""
-// let user_id_query = `id: "${user_id}",`
-let user_id_query = ""
-let users = gql`
-    {
-        users(first: 3, where: { ${user_id_query}borrowedReservesCount_gt: 0}){
-            id
-            borrowedReservesCount
-            collateralReserve:reserves (where: {currentATokenBalance_gt: 0}){
-                currentATokenBalance
-                reserve{
-                  usageAsCollateralEnabled
-                  reserveLiquidationThreshold
-                  reserveLiquidationBonus
-                  borrowingEnabled
-                  utilizationRate
-                  symbol
-                  underlyingAsset
-                  price {
-                    priceInEth
-                  }
-                  decimals
+
+
+const scanner = async() => {  
+
+  var count = 0;
+  var maxCount = 5; 
+  var onceAmount = 1000; 
+  
+  console.log(`\n${Date().toLocaleString()} fetching unhealthy loans\n`)
+  while( count < maxCount ) {
+    let lastTimeStamp = ~~(new Date().getTime() / 1000);
+    // let user_id = "0xf37680f16b92747ee8537a7e2ccb0e51a7c52a64"//collateral undifined
+    // let user_id = "0x934a83805958237abcbacd06054a12641799273d" //threshold error on contract  
+    // let user_id_query = `id: "${user_id}",`
+    let user_id_query = ""
+    let users = gql`
+        {
+            users(first: ${onceAmount}, skip: ${onceAmount*count}, orderBy: id, orderDirection: desc, where: { ${user_id_query}borrowedReservesCount_gt: 0}){
+                id
+                borrowedReservesCount
+                collateralReserve:reserves (where: {currentATokenBalance_gt: 0}){
+                    currentATokenBalance
+                    reserve{
+                      usageAsCollateralEnabled
+                      reserveLiquidationThreshold
+                      reserveLiquidationBonus
+                      borrowingEnabled
+                      utilizationRate
+                      symbol
+                      underlyingAsset
+                      price {
+                        priceInEth
+                      }
+                      decimals
+                    }
                 }
-            }
-            borrowReserve: reserves(where: {currentTotalDebt_gt: 0}) {
-                currentTotalDebt
-                reserve{
-                  usageAsCollateralEnabled
-                  reserveLiquidationThreshold
-                  borrowingEnabled
-                  utilizationRate
-                  symbol
-                  underlyingAsset
-                  price {
-                    priceInEth
-                  }
-                  decimals
+                borrowReserve: reserves(where: {currentTotalDebt_gt: 0}) {
+                    currentTotalDebt
+                    reserve{
+                      usageAsCollateralEnabled
+                      reserveLiquidationThreshold
+                      borrowingEnabled
+                      utilizationRate
+                      symbol
+                      underlyingAsset
+                      price {
+                        priceInEth
+                      }
+                      decimals
+                    }
                 }
             }
         }
-    }
-`;
+    `;
 
-let borrows = gql`
-  {
-    borrows(first: 10) {
-      user {
-        id
-      }
-      timestamp
-    }
-  }
-`;
-let repays = gql`
-  {
-    repays(first: 10) {
-      user {
-        id
-      }
-      timestamp
-    }
-  }
-`;
-let getUsdPriceEth = gql`
-{
-  priceOracle(id: "1") {
-    usdPriceEth
-  }
+
+
+    request(
+      "https://api.thegraph.com/subgraphs/name/aave/protocol-v3-polygon",
+      users
+    ).then((data) => {
+
+      const total_loans = data.users.length
+      const unhealthyLoans = parseUsers(data);
+      if ( unhealthyLoans.length > 0 )
+        console.log('UnhealthyLoan :',unhealthyLoans[0].user_id)
+      if ( total_loans > 0 ) 
+        console.log( `Records:${total_loans} Unhealthy:${unhealthyLoans.length}\n`)
+        // console.log(data.users
+        //   );
+    });
+
+    count++;    
+
+  } 
 }
-`;
-let userReserves = gql`
-    {
-      userReserves(
-        first:3
-      ) {
-        scaledATokenBalance
-        reserve {
-          id
-          underlyingAsset
-          name
-          symbol
-          decimals
-          liquidityRate
-          reserveLiquidationBonus
-          lastUpdateTimestamp
-          aToken {
-            id
-          }
-        }
-        usageAsCollateralEnabledOnUser
-        stableBorrowRate
-        stableBorrowLastUpdateTimestamp
-        principalStableDebt
-        scaledVariableDebt
-        variableBorrowIndex
-        lastUpdateTimestamp
-      }
-    }
-`;
 
-let reserveData = gql`
-    {
-      reserves(first: 1) {
-        id
-        underlyingAsset
-        name
-        symbol
-        decimals
-        isActive
-        isFrozen
-        usageAsCollateralEnabled
-        borrowingEnabled
-        stableBorrowRateEnabled
-        baseLTVasCollateral
-        optimalUtilisationRate
-        averageStableRate
-        stableRateSlope1
-        stableRateSlope2
-        baseVariableBorrowRate
-        variableRateSlope1
-        variableRateSlope2
-        variableBorrowIndex
-        variableBorrowRate
-        totalScaledVariableDebt
-        liquidityIndex
-        reserveLiquidationThreshold
-        aToken {
-          id
-        }
-        vToken {
-          id
-        }
-        sToken {
-          id
-        }
-        availableLiquidity
-        stableBorrowRate
-        liquidityRate
-        totalPrincipalStableDebt
-        totalLiquidity
-        utilizationRate
-        reserveLiquidationBonus
-        price {
-          priceInEth
-        }
-        lastUpdateTimestamp
-        stableDebtLastUpdateTimestamp
-        reserveFactor
-      }
-    }
-`;
-request(
-    // "https://api.thegraph.com/subgraphs/name/aave/protocol-v2",
-    "https://api.thegraph.com/subgraphs/name/aave/protocol-v3-polygon",
-    users
-).then((data) => {
-    console.log(data.users
-      );
-});
+function parseUsers( payload: { users: any[]; } ) {
 
+  const healthFactorMax = 1 //liquidation can happen when less than 1
+  var profit_threshold = 100 * (10**8) //in eth. A bonus below this will be ignored
+  const allowedLiquidation = .5 //50% of a borrowed asset can be liquidated
+
+  var loans: any[] =[];
+
+  payload.users.forEach((user, i) => {
+    var totalBorrowed=0;
+    var totalCollateral=0;
+    var totalCollateralThreshold=0;
+    var max_borrowedSymbol: any;
+    var max_borrowedDecimals: any;
+    var max_borrowedPrincipal=0;
+    var max_borrowedPriceInEth = 0;
+    var max_collateralSymbol: any;
+    var max_collateralBonus=0;
+    var max_collateralPriceInEth = 0;
+
+    user.borrowReserve.forEach((borrowReserve: { reserve: { price: { priceInEth: any; }; decimals: number; symbol: any; }; currentTotalDebt: any; }, i: any) => {
+      var priceInEth= borrowReserve.reserve.price.priceInEth
+      var principalBorrowed = borrowReserve.currentTotalDebt
+      totalBorrowed += priceInEth * principalBorrowed / (10**borrowReserve.reserve.decimals)
+      if (principalBorrowed > max_borrowedPrincipal){
+        max_borrowedSymbol = borrowReserve.reserve.symbol
+        max_borrowedDecimals = borrowReserve.reserve.decimals
+        max_borrowedPrincipal = principalBorrowed
+        max_borrowedPriceInEth = priceInEth
+      }        
+    });
+
+    user.collateralReserve.forEach((collateralReserve: { reserve: { price: { priceInEth: any; }; decimals: number; reserveLiquidationThreshold: number; reserveLiquidationBonus: number; symbol: any; }; currentATokenBalance: any; }, i: any) => {
+      var priceInEth= collateralReserve.reserve.price.priceInEth
+      var principalATokenBalance = collateralReserve.currentATokenBalance
+      totalCollateral += priceInEth * principalATokenBalance / (10**collateralReserve.reserve.decimals)
+      totalCollateralThreshold += priceInEth * principalATokenBalance * (collateralReserve.reserve.reserveLiquidationThreshold/10000)/ (10**collateralReserve.reserve.decimals)
+      if (collateralReserve.reserve.reserveLiquidationBonus > max_collateralBonus){
+        max_collateralSymbol = collateralReserve.reserve.symbol
+        max_collateralBonus=collateralReserve.reserve.reserveLiquidationBonus
+        max_collateralPriceInEth = priceInEth
+      }
+    });
+
+    var healthFactor = totalCollateralThreshold / totalBorrowed;
+    
+    // console.log('healthFactor', healthFactor)
+    // console.log('totalBorrowed', totalBorrowed)
+    // console.log('totalCollateralThreshold', totalCollateralThreshold)
+
+    if ( healthFactor <= healthFactorMax ) {
+     
+      loans.push( {
+        "user_id"  :  user.id,
+        "healthFactor"   :  healthFactor,
+        "max_collateralSymbol" : max_collateralSymbol,
+        "max_borrowedSymbol" : max_borrowedSymbol,
+        "max_borrowedPrincipal" : max_borrowedPrincipal,
+        "max_borrowedPriceInEth" : max_borrowedPriceInEth,
+        "max_collateralBonus" : max_collateralBonus/10000,
+        "max_collateralPriceInEth" : max_collateralPriceInEth
+      })
+
+    }
+
+    loans = loans.filter((loan) => {
+      return loan.max_borrowedPrincipal * allowedLiquidation * (loan.max_collateralBonus-1) * loan.max_borrowedPriceInEth / 10 ** max_borrowedDecimals >= profit_threshold
+    })
+
+  })
+
+  return loans;
+
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const excuteScan = async () => {
+  while( 1==1 ){
+    scanner();
+    await sleep(60_000);
+  }  
+}
+
+excuteScan();
+  
