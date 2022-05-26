c353bf38eab143cc5942ec9f6cc34e7d2d4cae87
loophe <loophe@163.com>
2022-05-26 16:58:32 +0800

    add fs.writeFile

--- aave_scanner_avalanche.ts
+++ aave_scanner_avalanche.ts
@@ -1,4 +1,8 @@
 import { request, gql } from "graphql-request";
+import fs from "fs";
+import path from "path";
+
+var table: any[] = [];
 
 const scanner = async() => {  
 
@@ -6,7 +10,7 @@ const scanner = async() => {
   var maxCount = 4; 
   var onceAmount = 1000; 
   
-  console.log(`\n${Date().toLocaleString()} fetching unhealthy loans\n`)
+  console.log(`\n${Date().toLocaleString()} fetching unhealthy loans`)
   while( count < maxCount ) {
     let lastTimeStamp = ~~(new Date().getTime() / 1000);
     // let user_id = "0xf37680f16b92747ee8537a7e2ccb0e51a7c52a64"//collateral undifined
@@ -60,20 +64,36 @@ const scanner = async() => {
       users
     ).then((data) => {
 
-      const total_loans = data.users.length
+      // const total_loans = data.users.length
       const unhealthyLoans = parseUsers(data);
-      if ( unhealthyLoans.length > 0 )
-        console.log('UnhealthyLoan :',unhealthyLoans[0].user_id)
-      if ( total_loans > 0 ) 
-        console.log( `Records:${total_loans} Unhealthy:${unhealthyLoans.length}\n`)
-        // console.log(data.users
-        //   );
+      if ( unhealthyLoans.length > 0 ){
+        console.log(unhealthyLoans) 
+        table.push( {"time": Date().toLocaleString(),  unhealthyLoans }) 
+        var content  = JSON.stringify(table)                           
+        fs.writeFile(path.join(__dirname, `tables/table.json`), content, err => {
+          if (err) {
+          console.error(err)
+          return
+          }
+          //file written successfully
+        })
+      }
+        // console.log('UnhealthyLoan :',unhealthyLoans[0].user_id)
+
+      // if ( total_loans > 0 ) 
+      //   console.log( `Records:${total_loans} Unhealthy:${unhealthyLoans.length}\n`)
+        
     });
 
     count++;    
 
   } 
 }
+
+// const blackAccountsList = [
+//   '0x633e5a89c2cc04076b13f340cbd374d564fc1f80',
+//   '0x697606200b3e87b35c96658ff877f538e0cf5d96'
+// ];
 
 function parseUsers( payload: { users: any[]; } ) {
 
@@ -111,7 +131,13 @@ function parseUsers( payload: { users: any[]; } ) {
       var priceInEth= collateralReserve.reserve.price.priceInEth
       var principalATokenBalance = collateralReserve.currentATokenBalance
       totalCollateral += priceInEth * principalATokenBalance / (10**collateralReserve.reserve.decimals)
-      totalCollateralThreshold += priceInEth * principalATokenBalance * (collateralReserve.reserve.reserveLiquidationThreshold/10000)/ (10**collateralReserve.reserve.decimals)
+
+      if ( collateralReserve.reserve.symbol == 'DAI.e' || collateralReserve.reserve.symbol == 'USDC' ){
+        totalCollateralThreshold += priceInEth * principalATokenBalance * ( 0.975 / (10**collateralReserve.reserve.decimals))//Token DAI.e and USDC threshold is 97.5%
+      }else{
+        totalCollateralThreshold += priceInEth * principalATokenBalance * (collateralReserve.reserve.reserveLiquidationThreshold/10000)/ (10**collateralReserve.reserve.decimals)
+      }
+      
       if (collateralReserve.reserve.reserveLiquidationBonus > max_collateralBonus){
         max_collateralSymbol = collateralReserve.reserve.symbol
         max_collateralBonus=collateralReserve.reserve.reserveLiquidationBonus
@@ -120,7 +146,7 @@ function parseUsers( payload: { users: any[]; } ) {
     });
 
     var healthFactor = totalCollateralThreshold / totalBorrowed;
-    
+    // console.log('userId', user.id)
     // console.log('healthFactor', healthFactor)
     // console.log('totalBorrowed', totalBorrowed)
     // console.log('totalCollateralThreshold', totalCollateralThreshold)
@@ -141,7 +167,26 @@ function parseUsers( payload: { users: any[]; } ) {
     }
 
     loans = loans.filter((loan) => {
-      return loan.max_borrowedPrincipal * allowedLiquidation * (loan.max_collateralBonus-1) * loan.max_borrowedPriceInEth / 10 ** max_borrowedDecimals >= profit_threshold
+      // blackAccountsList.forEach((blackAccount, i)=>{
+        // if ( loan.user_id != blackAccount )
+        if ( 
+          loan.user_id != '0x3b02c328cba97fe8d1edf82c6261abf9c6e4f585' && 
+          loan.user_id != '0xe36b0fcad85e29d59aeae916de66f5a93c8879b7' &&
+          loan.user_id != '0xcfa182c468876e79f4b4b6383f4226de6d985019' &&
+          loan.user_id != '0xf0d74983a35f07cff90b88d9ed71c03d03304d73' &&
+          loan.user_id != '0x77dc4b827300a4937146dfbd6049dfd2340ae66b' &&
+          loan.user_id != '0x77713f2c8f60f1678556e143adaf71fcee3825e9' &&
+          loan.user_id != '0x2bd7d71390ed6946159b1cb49bc270cbe3ef1a53' &&
+          loan.user_id != '0xfe238db1de2082bb9ba5dfc184b7e7c69c9be636' &&
+          loan.user_id != '0x01bfcd5bfe983ee701a28e12c4f7fc77dd4452fd' &&
+          loan.user_id != '0x682da57b6d854786b45848884c29a4d3c7b1ad03' &&
+          loan.user_id != '0xfd2d9fde6453b38b64d69fe3af0f442b254006ca' &&
+          loan.user_id != '0xa177fb36af688172a3527ae5b2f8489719e7c206' &&
+          loan.user_id != '0xe996fda4caa3c3f19649e012a5869642c31a8537' &&//error debt and collactrol amount
+          loan.user_id != '0xc2bfadf539272693ac53821d8ad4766bd9334d2d'
+          )
+        return loan.max_borrowedPrincipal * allowedLiquidation * (loan.max_collateralBonus-1) * loan.max_borrowedPriceInEth / 10 ** max_borrowedDecimals >= profit_threshold
+      // })      
     })
 
   })
